b5b438d5c61cf0b3364504fb0a6f24e47e7d7781
Jakub <kubawl.91@gmail.com>
2016-12-13 11:07:59 +0100

    repair def print menu

--- ui.py
+++ ui.py
@@ -64,8 +64,8 @@ def print_menu(title, list_options, exit_message):
 
     print(title + ':')
     for i in range(len(list_options)-1):
-        print('\t ({}) {}'.format(i+1, list_options[i])
-    print('\t (0) '+ exit_message)    
+        print('  ({}) {}'.format(i+1, list_options[i]))
+    print('  (0) '+ exit_message)
 
     pass
 
